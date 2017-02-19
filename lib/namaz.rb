db152b3f8a8b7b312375895d2527c9186b83296c
Sohair Ahmad <sohair1991@gmail.com>
2017-02-19 11:46:54 +0500

    Namaz: Added timings function for timingsByCity

--- lib/namaz.rb
+++ lib/namaz.rb
@@ -26,8 +26,28 @@ module Namaz
         method: method
       }
 
-      namaz_response = connection.get(namaz_url, params)
-      return Hashie::Mash.new(MultiJson.load(namaz_response.body)).data.timings if namaz_response.success?
+      namaz_response(namaz_url, params)
+    end
+
+    # Retrieve the namaz timings for a given city, country, method.
+    #
+    # @param latitude [Float] Latitude.
+    # @param longitude [Float] Longitude.
+    # @param timezonestring [String]
+    # @param method [Integer]
+    # @param options [Hash], Have optional values like timestamp, that is default to Current timestamp
+    def timings(city:, country:, method:, options: {})
+      timestamp = options[:timestamp] ? options[:timestamp] : Time.now.to_i
+      namaz_url = [DEFAULT_API_URL, 'timingsByCity', timestamp].join('/')
+
+      params = {
+        city: city,
+        country: country,
+        state: options[:state],
+        method: method
+      }
+
+      namaz_response(namaz_url, params)
     end
 
     # Build or get an HTTP connection object.
@@ -46,6 +66,11 @@ module Namaz
 
     def get(path, params = {})
       connection.get(path, params)
+    end
+
+    def namaz_response(namaz_url, params)
+      namaz_response = connection.get(namaz_url, params)
+      return Hashie::Mash.new(MultiJson.load(namaz_response.body)).data.timings if namaz_response.success?
     end
   end
 end
